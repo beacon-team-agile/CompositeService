5757d704ae37c5035ff1c4cac98a246637386d02
MarcoBackman <roni2006@hanmail.com>
2022-08-18 13:03:55 -0400

    Onboard form submission feature implemented

--- src/main/java/com/example/compositeservice/controller/EmployeeController.java
+++ src/main/java/com/example/compositeservice/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.example.compositeservice.controller;
 
+import com.example.compositeservice.domain.request.EmplyeeForm.OnBoardFormatRequest;
+import com.example.compositeservice.entity.EmployeeService.Employee;
+import com.example.compositeservice.service.CompositeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,8 +17,50 @@ import java.text.ParseException;
 @PreAuthorize("hasAuthority('employee')")
 public class EmployeeController {
 
+    private CompositeService compositeService;
+
+    @Autowired
+    public void setCompositeService(CompositeService compositeService) {
+        this.compositeService = compositeService;
+    }
+
     @GetMapping("/onboard")
-    public String viewOnBoardPage(HttpServletResponse response) throws ParseException, IOException {
+    public String viewOnBoardPage(HttpServletResponse response)
+            throws ParseException, IOException {
+
+        return "On board page";
+    }
+
+    @PostMapping("/onboard")
+    public String submitForm(OnBoardFormatRequest onBoardFormatRequest,
+                                  @RequestPart MultipartFile multiFile,
+                                  HttpServletResponse response)
+            throws ParseException, IOException {
+
+        //Add new employee data
+        Employee newEmployee = Employee.builder()
+                .firstName(onBoardFormatRequest.getFirstName())
+                .lastName(onBoardFormatRequest.getLastName())
+                .middleName(onBoardFormatRequest.getMiddleName())
+                .preferredName(onBoardFormatRequest.getPreferredName())
+                .email(onBoardFormatRequest.getEmail())
+                .cellPhone(onBoardFormatRequest.getCellPhone())
+                .alternatePhone(onBoardFormatRequest.getAlternatePhone())
+                .gender(onBoardFormatRequest.getGender())
+                .ssn(onBoardFormatRequest.getSSN())
+                .dob(onBoardFormatRequest.getDateOfBirth())
+                .startDate(onBoardFormatRequest.getStartDate())
+                .endDate(onBoardFormatRequest.getEndDate())
+                .driverLicense(onBoardFormatRequest.getDriverLicense())
+                .driverLicenseExpiration(onBoardFormatRequest.getDriverLicenseExpiration())
+                .houseId(Integer.valueOf(onBoardFormatRequest.getHouseId()))
+                .contact(onBoardFormatRequest.getContact())
+                .address(onBoardFormatRequest.getAddress())
+                .visaStatus(onBoardFormatRequest.getVisaStatus())
+                .personalDocument(onBoardFormatRequest.getPersonalDocument())
+        .build();
+
+        this.compositeService.addEmployeeForm(newEmployee, multiFile);
 
         return "On board page";
     }
