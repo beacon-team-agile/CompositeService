550691fc76753e74b42e9d12720f6ca6d82f7862
Yang Bo <VictorBoYang@gmail.com>
2022-08-18 11:11:00 -0400

    finished Hr approve/reject application and email

---
+++ src/main/java/com/example/compositeservice/domain/response/ApplicationResponse/SingleApplicationWorkFlowResponse.java
@@ -0,0 +1,16 @@
+package com.example.compositeservice.domain.response.ApplicationResponse;
+
+import com.example.compositeservice.domain.response.common.ResponseStatus;
+import com.example.compositeservice.entity.ApplicationService.ApplicationWorkFlow;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class SingleApplicationWorkFlowResponse {
+    private ResponseStatus responseStatus;
+
+    private ApplicationWorkFlow applicationWorkFlow;
+}
