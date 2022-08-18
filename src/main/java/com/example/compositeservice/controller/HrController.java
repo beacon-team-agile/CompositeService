3918fc4fc7246f9b9efdd99d009a89fa9cf872fd
Yang Bo <VictorBoYang@gmail.com>
2022-08-18 17:13:49 -0400

    composite generate token and email token

--- src/main/java/com/example/compositeservice/controller/HrController.java
+++ src/main/java/com/example/compositeservice/controller/HrController.java
@@ -1,14 +1,17 @@
 package com.example.compositeservice.controller;
 
 import com.example.compositeservice.domain.request.ApplicationService.EmailApplicationStatusRequest;
+import com.example.compositeservice.domain.request.AuthenticationService.TokenRequest;
 import com.example.compositeservice.domain.response.ApplicationResponse.SingleApplicationWorkFlowResponse;
 import com.example.compositeservice.domain.response.EmployeeResponse.AllEmployeesBriefInfoResponse;
 import com.example.compositeservice.domain.response.EmployeeResponse.EmployeeBriefInfoResponse;
 import com.example.compositeservice.domain.response.EmployeeResponse.SingleEmployeeResponse;
 import com.example.compositeservice.service.CompositeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -48,4 +51,24 @@ public class HrController {
         return compositeService.emailApplicationResultById(id, emailApplicationStatusRequest);
     }
 
+    @PostMapping("/credential/generateToken_sendEmail")
+    public ResponseEntity<String> generateToken_sendEmail(@RequestBody TokenRequest tokenRequest){
+        String emailTokenURI = "http://localhost:8088/authentication-service/credential/generate";
+
+        String accessToken = tokenRequest.getToken();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        RequestEntity<TokenRequest> requestEntity = RequestEntity
+                .post(emailTokenURI, tokenRequest)
+                .headers(headers)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(tokenRequest);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
+
+        return response;
+    }
 }
